73a2417d1641641786b965a1fb8ed7cc8a5e4430
akjpeg <aureliokenjikanashiro@gmail.com>
2025-03-25 23:57:20 -0300

    faet: add migration to ensure uniqueness of phone number

--- ASPNETCoreBackend/Migrations/20250326023118_AddUniqueConstraintToPhoneOnClientEntity.Designer.cs
+++ ASPNETCoreBackend/Migrations/20250326023118_AddUniqueConstraintToPhoneOnClientEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ASPNETCoreBackend.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ASPNETCoreBackend.Migrations
 {
     [DbContext(typeof(ParkingLotDbContext))]
-    partial class ParkingLotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250326023118_AddUniqueConstraintToPhoneOnClientEntity")]
+    partial class AddUniqueConstraintToPhoneOnClientEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ASPNETCoreBackend/Migrations/20250326023118_AddUniqueConstraintToPhoneOnClientEntity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPNETCoreBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraintToPhoneOnClientEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_clients_PhoneNumber",
+                schema: "parking_lot_system",
+                table: "clients",
+                column: "PhoneNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_clients_PhoneNumber",
+                schema: "parking_lot_system",
+                table: "clients");
+        }
+    }
+}
